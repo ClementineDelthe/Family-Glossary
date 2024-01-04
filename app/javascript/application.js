56233d8d7ac7fcbb45a251f23b7cbb5ae02a8dd0
clementine delthe <clementine.delthe@gmail.com>
2024-01-04 16:28:48 +0100

    JS fixing

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "./controllers"
+import "controllers"
 import "@popperjs/core"
 import "bootstrap"
-// import { Application } from "stimulus"
-// window.Stimulus = Application.start()
